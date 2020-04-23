38177752ed13e15faa8922468e53596bbc5e82eb
zhaoshichen <zhaoshichen1@gmail.com>
2020-04-23 21:51:01 +0800

    add

--- handle/hello.go
+++ handle/hello.go
@@ -5,33 +5,6 @@ import (
 	"nakama-golang/protocol"
 )
 
-func helloHandle(t *fantasy.Tifa) {
-	req := &protocol.ReqHello{}
-	if err := t.Bind(req); err != nil {
-		t.Abort()
-		return
-	}
-	t.Json(ser.Hello(t.Ctx, t.Logger, t.Db, t.Nk, req))
-}
-
-func matchHandle(t *fantasy.Tifa) {
-	req := &protocol.ReqMatchJoin{}
-	if err := t.Bind(req); err != nil {
-		t.Abort()
-		return
-	}
-	t.Json(ser.MatchJoin(t.Ctx, t.Logger, t.Db, t.Nk, req))
-}
-
-func matchReady(t *fantasy.Tifa) {
-	req := &protocol.ReqMatchReady{}
-	if err := t.Bind(req); err != nil {
-		t.Abort()
-		return
-	}
-	t.Json(ser.MatchReady(t.Ctx, t.Logger, t.Db, t.Nk, req))
-}
-
 func gameReady(t *fantasy.Tifa) {
 	req := &protocol.ReqGameReady{}
 	if err := t.Bind(req); err != nil {
